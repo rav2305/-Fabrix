91718556f39d3d004b63c224a35e423bbfb7964f
rav2305 <rav2305@users.noreply.github.com>
2026-05-17 15:23:57 +0530

    fix config typo

--- fabrix_shop_manager/config.py
+++ fabrix_shop_manager/config.py
@@ -29,8 +29,7 @@ def _build_database_uri() -> str:
 class Config:
     SECRET_KEY = SECRET_KEY
     SQLALCHEMY_DATABASE_URI = _build_database_uri()
-    SQLALCHEMY_TRACK_MODIFICATIONS = False_URL.rstrip("/")
-
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 def get_config():
     return Config
